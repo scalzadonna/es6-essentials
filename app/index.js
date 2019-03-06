ea47656f19c9c13bdf8093d777767221df579c0b
Santi Scalzadonna <pilujander@gmail.com>
2019-03-06 16:43:40 -0300

    Chapter 3, lesson 20

--- app/index.js
+++ app/index.js
@@ -1,7 +1,24 @@
-function butter(...b) {
-    let a = [1, 2, 3];
-    return [...a,...b];
-  }
-  
-console.log(  butter(4, 5, 6));
-  
+let c = [100,200];
+
+let a = c[0];
+let b = c[1];
+
+console.log(a,b);
+
+let [d,e] = c;
+
+console.log(d,e);
+
+let fellowship = ['Frodo', 'Gandalf', 'Aragorn'];
+
+let [hobbit, wizard, ranger] = fellowship;
+
+console.log(hobbit, wizard, ranger);
+
+
+let f = [100,200,300,400,500];
+
+
+let [g, ...h] = f;
+
+console.log(g,h);
