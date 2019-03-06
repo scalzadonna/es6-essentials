bff4d8901d4d0d1706dad330f3c2317b9c7d784c
Santi Scalzadonna <pilujander@gmail.com>
2019-03-06 16:38:52 -0300

    Chapter 3, lesson 19, exc1

--- app/index.js
+++ app/index.js
@@ -1,19 +1,7 @@
-let a = [7,8,9];
-let b = [6, ...a, 10];
-
-//console.log(b);
-
-function print(a,b,c){
-    console.log(a,b,c);
-}
-
-let z = [1,2,3];
-//print(z[0],z[1],z[2]);
-
-//print(...z);
-
-function print2(...z){
-    console.log(z);
-}
-
-print2(1,2,3,4,5,6);
+function butter(...b) {
+    let a = [1, 2, 3];
+    return [...a,...b];
+  }
+  
+console.log(  butter(4, 5, 6));
+  
